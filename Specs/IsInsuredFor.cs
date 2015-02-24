9ef72e1b2aaa4a913ff0a2e9e61800f33b3abe4f
'jvinyard' <jvinyard@uship.com>
2015-02-24 12:42:59 -0600

    Add another spec with a decimal type.  The formatting doesn't quite work.

---
+++ Specs/IsInsuredFor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using SpecUI.Attributes;
+using SpecUI.Models;
+
+namespace SpecUI.Specs
+{
+    [Description("The shipment is insured for a minimum amount")]
+    public class IsInsuredFor : BaseShipmentSpec
+    {
+        [Description("The minimum insurance amount")]
+        public decimal MinimumAmount { get; set; }
+
+        public override bool IsSatisfied(Shipment shipment)
+        {
+            return shipment.InsuranceAmount >= MinimumAmount;
+        }
+
+        public override IEnumerable<IValidationError> IsValid()
+        {
+            if (MinimumAmount <= 0)
+            {
+                yield return new ValidationError(
+                    message: "amount must be greater than zero",
+                    criterionName:() => MinimumAmount);
+            }
+        }
+    }
+}
